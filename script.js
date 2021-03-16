5643abcb4f1c297e6f91896a71e2c291f0658d4e
muca <turaliccmustafa@gmail.com>
2021-03-17 00:07:07 +0100

    bug fixes

--- script.js
+++ script.js
@@ -15,12 +15,10 @@ document.querySelector('.check').addEventListener('click', function () {
   // When there is no input
   if (!guess) {
     displayMessage('⛔️ No number!');
-
     // When player wins
   } else if (guess === secretNumber) {
     displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
-
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -40,8 +38,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
 
- 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
